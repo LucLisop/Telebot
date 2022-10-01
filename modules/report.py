0854f4395151fd1ffb8e60a1f41828f23df8b126
Abominate34 <92999356+Abominate34@users.noreply.github.com>
2022-10-01 15:01:04 +0300

    Add files via upload

---
+++ modules/report.py
@@ -0,0 +1,60 @@
+# Report
+'''
+.report:: Жалоба на чат\n\n<b>Использование</b>: \n<code>.report</code> <Комментарий к жалобе>(по желанию)
+'''
+import asyncio
+from telethon import events
+from asyncio import sleep
+import random
+from telethon import events, types, functions, errors, TelegramClient
+
+
+reasons = [
+types.InputReportReasonChildAbuse(),
+types.InputReportReasonCopyright(),
+types.InputReportReasonFake(),
+types.InputReportReasonPornography(),
+types.InputReportReasonSpam(),
+types.InputReportReasonViolence(),
+types.InputReportReasonOther()]
+
+
+
+def a(client):
+	@client.on(events.NewMessage(pattern=r"\.report", outgoing=True))
+	async def _(event):
+		print(event.stringify())
+		args = str(event.message.text).split(' ')
+
+		if len(args) < 2:
+			comment = 'Delete chat!'
+		else:
+			comment = args[-1]
+
+		for reason in reasons:
+			
+			result = await client(
+				functions.account.ReportPeerRequest(
+					peer=event.peer_id,
+					reason=reason,
+					message=comment
+				)
+			)
+			try:
+				await event.edit(f'Жалоба {str(reason)} отправлена!')
+			except Exception as e:
+				print(e)
+
+		await event.edit(f'<b>Жалобы успешно отправлены!</b>',parse_mode='html')
+
+
+		
+		
+ 
+
+
+if __name__ == '__main__':
+	try:
+		a(client)
+	except:
+		pass
